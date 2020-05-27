f756bb3ebb878ca7ecbb921a79f67951c26b3422
Vicky <kosenchiha@gmail.com>
2020-05-27 23:24:37 +0100

    Create Order schema

---
+++ chowpronto_backend/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Order = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+
+  cart: [
+    {
+      menuItemId: { type: mongoose.Schema.ObjectId, ref: "MenuItem" },
+      quantity: { type: Number },
+    },
+  ],
+
+  patronId: { type: mongoose.Schema.ObjectId, ref: "Patron" },
+
+  deliveryDetails: {
+    firstName: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    secondName: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 1024,
+      min: 6,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+  },
+});
+
+module.exports = mongoose.model("Order", Order);
