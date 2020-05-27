b7eee8bfa0bcf9ebdee7e8b23780a2833a85ac99
Matt Player <mattplayer@mac.com>
2020-05-27 15:46:26 +0100

    added total to order details & fixed unit quantities

--- chowpronto_frontend/src/patron/pages/confirmOrder/components/OrderDetails.js
+++ chowpronto_frontend/src/patron/pages/confirmOrder/components/OrderDetails.js
@@ -12,7 +12,7 @@ export default function OrderDetails() {
     { name: "french fries", unitPrice: 595, quantity: 10 },
   ];
   const deliveryAddress = "AB12 3EY";
-  const deliveryDate = new Date(2020, 4, 30);
+  const deliveryDate = new Date(2020, 4, 29, 18, 30);
   return (
     <StyledOrderDetails>
       <h3>Order Details</h3>
@@ -23,10 +23,19 @@ export default function OrderDetails() {
             {".".repeat(
               30 - val.name.length - Math.floor(Math.log10(val.quantity))
             )}{" "}
-            {parseMoney(val.unitPrice)}
+            {parseMoney(val.unitPrice * val.quantity)}
           </p>
         );
       })}
+      <p>
+        <strong>
+          total
+          {".".repeat(30)}{" "}
+          {parseMoney(
+            foodList.reduce((acc, val) => acc + val.unitPrice * val.quantity, 0)
+          )}
+        </strong>
+      </p>
       <h4>Delivering to: {deliveryAddress}</h4>
       <h4>{parseTime(deliveryDate)}</h4>
       <br />
