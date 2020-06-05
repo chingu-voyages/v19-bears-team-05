d148e0729fa5077a4f9ad6fde24ff9ad2b9932ea
Matt Player <mattplayer@mac.com>
2020-06-05 18:23:05 +0100

    Co-authored-by: Viktoriia <kosenchiha@gmail.com>

--- chowpronto_frontend/src/hooks/useAuth.js
+++ chowpronto_frontend/src/hooks/useAuth.js
@@ -1,45 +1,52 @@
+import { useContext, useState, useEffect } from "react";
+import UserContext from "../state/UserContext";
+
 function useAuth() {
-  // takes no arguments and returns 3 functions, get current user (getUser()), login & logout
-  // const fakeGuest = {
-  //   email: "fakeguest@gmail.com",
-  //   name: "New Patron",
-  //   password: "",
-  //   phone: "+12-3457-8910",
-  //   address: "123 Flat, 12 Hope Street, Faith City, Wanderland",
-  //   postcode: "W 765 HS",
-  //   role: "GUEST",
-  // };
-  const fakeUser = {
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Z…4NDN9.5dQx6aYVjwuwHFKcfNSjVHYfr_BwPXq-A_7luNZWGQ0",
-    patron: {
-      email: "fakeguest@gmail.com",
-      name: "New Patron",
+  const { user, setUser } = useContext(UserContext);
+  async function onInit() {
+    const storageData = getFromStorage();
+    const userDetails = await getUserById(storageData.token);
+    setUserDetailsToContext(userDetails);
+  }
+  function getUserById(token) {
+    // fetch("/api/patron/getUserById", {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+    return {
+      _id: "5ed935717d520e32d44787b1",
+      name: "Test Patron",
+      emil: "test111@gmail.com",
       phone: "+12-3457-8910",
       address: "123 Flat, 12 Hope Street, Faith City, Wanderland",
       postcode: "W 765 HS",
-    },
-  };
-  let user = undefined;
-  const getUser = () => {
-    // return getFromStorage();
-    return fakeUser;
-  };
-  function setUser(dataObj) {
-    setToStorage(dataObj);
+    };
   }
-  const login = async (email, password) =>
-    new Promise((res, rej) =>
-      setTimeout(() => res({ name: "Kenny Loggins", id: "232323" }))
-    );
-  const logout = async () =>
-    setTimeout(() => {
-      user = null;
-      return true;
-    }, 200);
+  function login(email, password) {
+    const credentials = JSON.stringify({ email, password });
+    fetch("/api/patrons/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: credentials,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // setUser(data);
+      });
+  }
+  function logout() {
+    window.localStorage.removeItem("chowpronto");
+  }
+
+  function register(customerDetailsObject) {}
   function setToStorage(dataObj) {
     try {
-      window.localStorage.setItem("chowpronto", JSON.stringify(dataObj));
+      window.localStorage.setItem("chowpronto", JSON.stringify(dataObj.token));
     } catch (err) {
       console.log("err", err);
     }
@@ -52,7 +59,16 @@ function useAuth() {
       console.log("err", err);
     }
   }
-  return { getUser, setUser, login, logout };
+  function setUserDetailsToContext(userDetails) {
+    setUser({ type: "set_user", userDetails });
+  }
+  return {
+    // getUser,
+    onInit,
+    login,
+    logout,
+    register,
+  };
 }
 
 export default useAuth;
