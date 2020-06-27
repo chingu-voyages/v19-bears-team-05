1054af5e12d4c877de99e391e92b20943428bf8d
Vicky <kosenchiha@gmail.com>
2020-06-27 23:31:37 +0100

    Create fetch function to delete patron's account

---
+++ chowpronto_frontend/src/api/deleteAccount.js
@@ -0,0 +1,19 @@
+//pi / patrons / deleteprofile /:id
+
+function deleteAccount(token, _id) {
+  return fetch(`/api/patrons/profile/${_id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.status >= 200 && res.status < 300) {
+      return res.json();
+    } else {
+      throw res;
+    }
+  });
+}
+
+export default deleteAccount;
